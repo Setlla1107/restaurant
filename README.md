edc19630d9c88f85d851e6806eb3353a528efbbc
Setlla1107 <meruku13@gmail.com>
2022-08-28 14:31:34 +0800

    main

---
+++ README.md
@@ -0,0 +1,47 @@
+# 餐廳清單
+![Index page about Restaurant List](./public/image/restaurant.png)
+
+## 介紹
+可搜尋餐廳，觀看個別餐廳的詳細資訊
+
+### 功能
+- 檢視餐廳詳細資訊包含類別、地址、電話、評分、圖片及 Google Map
+- 可搜尋餐廳名稱與類別
+
+
+## 開始使用
+1. 請先確認本地環境已安裝 Node.js 與 npm 。
+
+2. 將專案 clone 至本地，至終端機輸入：
+   ```
+   git clone https://github.com/Setlla1107/restaurant.git
+   ```
+
+3. 在本地開啟之後，透過終端機進入資料夾，輸入：
+   ```
+   npm install
+   ```
+ 
+4. 安裝完成後，輸入：
+   ```
+   npm run dev
+   ```
+   
+5. 在終端機看見以下資訊，表示專案成功在本地運行了。
+   ```
+   Express is listening on http://localhost:3000
+   ```
+   
+6. 打開瀏覽器，輸入 "http://localhost:3000" ，即可瀏覽此專案。
+
+7. 停止使用，至終端機：
+   ```
+   ctrl + c
+   ```
+
+## 開發工具
+- Node.js v14.16.0
+- Express v4.16.4
+- Express-handlebars v3.0.0
+- Bootstrap v4.3.1
+- Font-awesome 5.8.1
---
+++ app.js
@@ -0,0 +1,48 @@
+// 用require載入express
+const express = require('express')
+const app = express()
+const port = 3000
+// 將JSON檔載入express中
+const restaurantList = require("./restaurant.json").results
+
+// 用require載入express-handlebars，定義樣板引擎
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// 設定express靜態檔案路由
+app.use(express.static('public'))
+
+
+// 將資料帶入index列表樣板
+app.get('/', (req, res) => {
+  res.render('index', { restaurantList })
+})
+
+// 將資料帶入show個別樣板
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.find(
+    restaurant => restaurant.id.toString() === req.params.restaurant_id
+  )
+  res.render('show', { restaurant: restaurant })
+})
+
+
+// 設定搜尋路由
+app.get('/search', (req, res) => {
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+
+  const restaurants = restaurantList.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
+  })
+
+  res.render('index', { restaurantList: restaurants, keywords })
+})
+
+
+
+// 啟動伺服器去監聽
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
